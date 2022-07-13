ddb6aaaa6817ff2997b634c188a0c29a8ed4e4dc
Mauro Almiron <mdavid.almiron@gmail.com>
2022-07-13 20:25:23 -0300

    Seteado espanol por defecto, agregamos el archivo de traduccion

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module GestorDocumentario
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.i18n.default_locale = :es
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
       YAML.load(File.open(env_file)).each do |key, value|
